b68e91777d3447b7e9ed3a702d8137503c7d78c5
TheSphereWizard <brian.horse@gmail.com>
2018-04-01 14:14:31 -0400

    filereading

---
+++ DungeonCrawlerTempName/src/Level.java
@@ -0,0 +1,11 @@
+
+public class Level {
+	//Not Today but Plan:
+	//Start is center -Rand room or special center room
+	//on each "entrance" -need special tile for this -not sure how nultiple wide doors will work
+	//try and place random room, if it fits then place it and continue else try
+	//another room type, if all rooms are invalid give up and replace with wall
+	
+	//also need to have outer bound, probably just square around level
+	
+}
