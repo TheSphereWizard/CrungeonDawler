fefd5d2007cf9b9d3a1a38d5adc5ab4684c22589
TheSphereWizard <brian.horse@gmail.com>
2018-04-16 22:59:16 -0400

    Hey I found a work around for importing the project into eclipse, after it gives you the error, just cancel and do the regular non git import existing repository and it will set it up with git/team automatically

--- CrungeonDawler/src/crungeonDawler/Window.java
+++ CrungeonDawler/src/crungeonDawler/Window.java
@@ -7,6 +7,7 @@ public class Window extends JFrame{
 		new Window(1388,768);//screen dimensions
 	}
 	static JFrame frame;//needed to close the frame
+	//Hey I found a work around for importing into eclipse
 	Window(int width, int height){
 		Window.frame=this;
 		this.setSize(width, height);
